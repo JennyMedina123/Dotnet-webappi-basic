2bdf7839829bc84a3a95c9016cb2c26b8a141eb5
jenny medina <jennymedina315551@correo.itm.edu.co>
2023-10-04 17:07:50 -0500

    inicial

---
+++ TODOApp/TODO.WebApi/Controllers/TODOItemsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TODO.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TODOItemsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<String> Get() 
+        {
+            return new string[] { "value 1", "value 2" };
+        }
+  // Get api/<TODOItemsController>5
+        [HttpGet("{id}")]
+        public String Get(int id) 
+        {
+            return "value";
+        }
+        // POST api/<TODOItemsController>
+        [HttpPost]
+        public void Post([FromBody] String value) 
+        {
+        }
+        // PUT api/<TODOItemsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) 
+        {
+        }
+        // DELETE api/<TODOItemsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) 
+        {
+        }
+
+    }
+}
---
+++ TODOApp/TODO.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TODO.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController(){
+
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetHelloWorld() {
+            return Ok("Hello World");
+
+        }
+            
+    }
+}
